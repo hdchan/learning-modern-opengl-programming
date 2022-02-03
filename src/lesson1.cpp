acc2570c1bf3d07f98f45682068601fdcf3161b5
Henry Chan <henry.d.chan@gmail.com>
2022-02-03 15:04:21 -0500

    Update libs for x64

--- src/lesson1.cpp
+++ src/lesson1.cpp
@@ -5,15 +5,56 @@ using namespace std;
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hellow Window 1";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
 
 int main() {
-
+    !glewInit();
     if (!glfwInit()) {
         cerr << "GLFW initialization failed" << endl;
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+    if (pWindow == NULL) {
+        cerr << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(pWindow);
+
+    // glfwSetKeyCallback(pWindow, glfw_onKey);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        cerr << "GLEW initialization failed" << endl;
+        return -1;
+    }
+
+    // main loop
+    while (!glfwWindowShouldClose(pWindow)) {
+        // polls for input events
+        // callback funtions
+        glfwPollEvents();
 
 
+        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // two buffers 
+        // 1) the one showing and the 
+        // 2) back buffer where we are drawing
+        // removes flicker/tearing
+        glfwSwapBuffers(pWindow); 
+    }
+
+    glfwTerminate();
     return 0;
 }
