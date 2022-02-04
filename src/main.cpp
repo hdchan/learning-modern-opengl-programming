93bf1b9351b462a4e33577851bcdfcb55a0b8c4a
Henry Chan <henry.d.chan@gmail.com>
2022-02-03 23:47:56 -0500

    Draw quad

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,22 @@ const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Triangle";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
 GLFWwindow* gWindow = NULL;
-
+bool gWireframe = false;
 
 const GLchar* vertextShaderSrc =
 "#version 330 core\n"
 "layout (location = 0) in vec3 pos;"
-"layout (location = 1) in vec3 color;"
-"out vec3 vert_color;"
 "void main()"
 "{"
-"   vert_color = color;"
 "   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
 "}";
 
 const GLchar* fragmentShaderSrc =
 "#version 330 core\n"
-"in vec3 vert_color;"
 "out vec4 frag_color;"
 "void main()"
 "{"
-"   frag_color = vec4(vert_color, 1.0f);"
+"   frag_color = vec4(0.35f, 0.96f, 0.3f, 1.0f);"
 "}";
 
 // function prototypes
@@ -43,32 +39,25 @@ int main() {
         return -1;
     }
 
-    GLfloat vert_pos[] = {
+    GLfloat verticies[] = {
         // position (x, y, z)   
-         0.0f,  0.5f,  0.0f, // top
-         0.5f, -0.5f,  0.0f, // right
-        -0.5f, -0.5f,  0.0f // left
-    };
+        -0.5f,  0.5f,  0.0f, 
+        0.5f, 0.5f,  0.0f,
+        0.5f, -0.5f,  0.0f,
 
-    GLfloat vert_color[] = {
-        // color (r, g, b) 
-        1.0f, 0.0f, 0.0f, // top
-        0.0f, 1.0f, 0.0f, // right
-        0.0f, 0.0f, 1.0f // left
+        -0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
     };
 
     // video buffer object and video array object
-    GLuint vbo, vbo2, vao; 
+    GLuint vbo, vao; 
 
     // get an address on the video card buffer and assign to vbo
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     // GL_STATIC_DRAW -> our veriticies won't change much
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vert_pos), vert_pos, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &vbo2);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vert_color), vert_color, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
@@ -76,16 +65,8 @@ int main() {
     // position attributes
     // Tells the layout of our verticies array
     // first parameter where vertextShaderSrc (location = 0)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo); // we switch to our first buffer because we can only work with on buffer at a time
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(0);
-
-    // color attributes, need to offset by 12 bytes
-    // first parameter "1" coincides with (location = 1)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2); // now activate the second buffer
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(1);
-
 
     // vertex shader
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
@@ -137,7 +118,7 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
         // two buffers 
@@ -193,6 +174,16 @@ void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
     // will be triggered when a key is pressed
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+        gWireframe = !gWireframe;
+        if (gWireframe) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
     }
 }
 
